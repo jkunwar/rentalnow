9eb403057b6cef0957ba4e702a53ccbe183be96a
jkunwar <jagdish21.3c@gmail.com>
2020-07-25 01:14:03 +0545

    set root url for serve

--- resources/js/src/config.js
+++ resources/js/src/config.js
@@ -3,8 +3,10 @@
  */
 const config = {
   env: process.env.NODE_ENV,
-  rootURI: 'http://localhost:8000/api/v1',
-  baseURI: 'http://localhost:8000'
+  // rootURI: 'http://localhost:8000/api/v1',
+  // baseURI: 'http://localhost:8000'
+  rootURI: 'http://rentalnow.kunwarj.com.np/api/v1',
+  baseURI: 'http://rentalnow.kunwarj.com.np'
 };
 
 export default config;
